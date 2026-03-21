ac299b464289091000b5d6603d1a802ed3be442a
quyen-dx <daoxuanquyen333@gmail.com>
2026-03-21 20:44:17 +0700

    ahi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { Main } from './page/main/main';
 import { Detail } from './page/detail/detail';
 import { Category } from './page/category/category';
+import { Cart } from './page/cart/cart';
 export const routes: Routes = [
   { path: '', component: Main },
   { path: 'detail/:id', component: Detail },
   { path: 'category', component: Category },
+  { path: 'cart', component: Cart },
 ];
