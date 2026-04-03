01b0c03f03b6afff09b884ab6987ca5a6f4f2dcc
quyen-dx <daoxuanquyen333@gmail.com>
2026-04-03 14:33:09 +0700

    ahihi

--- src/AppTheme.tsx
+++ src/AppTheme.tsx
@@ -1,23 +1,23 @@
-import { ConfigProvider, theme as themeAntd } from "antd";
-import { useContext } from "react";
-import App from "./App";
-import { ThemeContext } from "./context/ThemeContext";
+// import { ConfigProvider, theme as themeAntd } from "antd";
+// import { useContext } from "react";
+// import App from "./App";
+// import { ThemeContext } from "./context/ThemeContext";
 
-export default function AppTheme() {
-  const context = useContext(ThemeContext);
-  if (!context) return null;
+// export default function AppTheme() {
+//   const context = useContext(ThemeContext);
+//   if (!context) return null;
 
-  const { theme } = context;
+//   const { theme } = context;
 
-  return (
-    <ConfigProvider
-      theme={{
-        algorithm: theme === 'dark' ? themeAntd.darkAlgorithm : themeAntd.defaultAlgorithm
-      }}
-    >
-      <div className={theme === "dark" ? "dark" : "light"}>
-        <App />
-      </div>
-    </ConfigProvider>
-  );
-}
+//   return (
+//     <ConfigProvider
+//       theme={{
+//         algorithm: theme === 'dark' ? themeAntd.darkAlgorithm : themeAntd.defaultAlgorithm
+//       }}
+//     >
+//       <div className={theme === "dark" ? "dark" : "light"}>
+//         <App />
+//       </div>
+//     </ConfigProvider>
+//   );
+// }
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "./context/ThemeContext";
-import { UserProvider } from "./context/UserContext";
+// import { ThemeProvider } from "./context/ThemeContext";
+// import { UserProvider } from "./context/UserContext";
 import "./index.css";
-import AppTheme from "./AppTheme";
+import App from "./App";
+
+// import AppTheme from "./AppTheme";
 
 const queryClient = new QueryClient();
 
@@ -13,11 +15,11 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <UserProvider>
-          <ThemeProvider>
-             <AppTheme />
-          </ThemeProvider>
-        </UserProvider>
+        {/* <UserProvider>
+          <ThemeProvider> */}
+             <App />
+          {/* </ThemeProvider>
+        </UserProvider> */}
       </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
